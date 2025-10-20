65d67843c6b75e7c1b73030323bc078eb9ac0766
selee1012 <dltjddms1012@gmail.com>
2025-10-20 22:05:37 +0900

    [문자열 덧셈 계산기] 이성은 미션 제출합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,30 +8,36 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inp = sc.nextLine();
-        List<Character> target = new ArrayList<>(Arrays.asList(',', ':'));
-        if (inp.length() >= 5) {
-            if ((inp.charAt(0) == '/') && (inp.charAt(1) == '/') && (inp.charAt(3) == '\\') && (inp.charAt(4) == 'n')) {
-                target.add(inp.charAt(2));
-                inp = inp.substring(5);
+        inp = inp.trim();
+
+        if (inp.length() == 0)
+            System.out.println("결과 : 0");
+        else{
+            List<Character> target = new ArrayList<>(Arrays.asList(',', ':'));
+            if (inp.length() >= 5) {
+                if ((inp.charAt(0) == '/') && (inp.charAt(1) == '/') && (inp.charAt(3) == '\\') && (inp.charAt(4) == 'n')) {
+                    target.add(inp.charAt(2));
+                    inp = inp.substring(5);
+                }
             }
-        }
 
-        for (Character c : target) {
-             inp = inp.replace(c.toString(), " ");
-        }
-
-        String[] result = inp.split(" ");
-
-        int count = 0;
-        try {
-            for (int i=0; i<result.length; i++) {
-                count += Integer.parseInt(result[i]);
+            for (Character c : target) {
+                inp = inp.replace(c.toString(), ",");
             }
-        } catch (Exception e) {
-            throw new IllegalArgumentException("잘못된 입력입니다");
-        }
 
-        System.out.println("결과 : " + count);
+            String[] result = inp.split(",");
+
+            int count = 0;
+            try {
+                for (int i=0; i<result.length; i++) {
+                    count += Integer.parseInt(result[i]);
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException("잘못된 입력입니다");
+            }
+
+            System.out.println("결과 : " + count);
+        }
         sc.close();
     }
 }
