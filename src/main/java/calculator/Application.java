6e1e55905e1ff9933ceb10bc679d10e5d96bfd73
selee1012 <dltjddms1012@gmail.com>
2025-10-20 21:14:37 +0900

    1트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+
+        String inp = sc.nextLine();
+        List<Character> target = new ArrayList<>(Arrays.asList(',', ':'));
+        if (inp.length() >= 5) {
+            if ((inp.charAt(0) == '/') && (inp.charAt(1) == '/') && (inp.charAt(3) == '\\') && (inp.charAt(4) == 'n')) {
+                target.add(inp.charAt(2));
+                inp = inp.substring(5);
+            }
+        }
+
+        for (Character c : target) {
+             inp = inp.replace(c.toString(), " ");
+        }
+
+        String[] result = inp.split(" ");
+
+        int count = 0;
+        try {
+            for (int i=0; i<result.length; i++) {
+                count += Integer.parseInt(result[i]);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+
+        System.out.println(count);
+        sc.close();
     }
 }
